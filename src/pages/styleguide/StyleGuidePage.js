e55df4fec80b8e131c57c718a9cbcebf35e7219e
Sankalp Gunturi <sgunturi@cmu.edu>
2023-12-03 16:39:55 -0800

    Add typography, btns, input for styleguide

--- src/pages/styleguide/StyleGuidePage.js
+++ src/pages/styleguide/StyleGuidePage.js
@@ -1,8 +1,72 @@
 import React from "react";
+import { Button, Typography, Checkbox, Form, Input } from "antd";
 
 const StyleGuidePage = () => {
+  const { Title } = Typography;
   return (
-    <div>This is style guide page taken from ant design and tailwind css</div>
+    <div>
+      <div>
+        <h1>Style Guide</h1>
+        {/* TBD: <h2>Colors</h2> */}
+        <h2>Typography</h2>
+        <Title>Heading 1</Title>
+        <Title level={2}>Heading 2</Title>
+        <Title level={3}>Heading 3</Title>
+        <Title level={4}>Heading 4</Title>
+        <Title level={5}>Heading 5</Title>
+        <h2>Buttons</h2>
+        <Button type="primary" size="large">
+          Primary Button
+        </Button>
+        <Button size="large">Secondary Button</Button>
+        <Button danger type="text">
+          Tertiary Button
+        </Button>
+        <h2>Inputs</h2>
+        <div>
+          <p>Checkboxes</p>
+          <Checkbox>Checkbox (active)</Checkbox>
+          <Checkbox defaultChecked disabled>
+            Checkbox (inactive)
+          </Checkbox>
+        </div>
+        <div>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            autoComplete="off"
+          >
+            <p>Input (regular)</p>
+            <Form.Item
+              label="Username"
+              name="username"
+            >
+              <Input />
+            </Form.Item>
+            <p>Input (error)</p>
+            <Form.Item
+              label="Username"
+              name="username"
+              validateStatus="error"
+              help="Please input your username!"
+            >
+              <Input />
+            </Form.Item>            
+          </Form>
+        </div>
+      </div>
+    </div>
   );
 };
 
